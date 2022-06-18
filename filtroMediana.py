54c466cd9cfd83ad2bda7eb3506d68f587f881f1
tshelena <helena.tavares@sou.ucpel.edu.br>
2022-06-17 21:23:55 -0300

    mediana

--- filtroMediana.py
+++ filtroMediana.py
@@ -1,24 +1,24 @@
 import cv2 
 import numpy as np  
 
-img_noisy1 = cv2.imread('chile.jpg', 0) 
+img = cv2.imread('chile.jpg', 0) 
 m, n = img_noisy1.shape 
    
-img_new1 = np.zeros([m, n]) 
+img_new = np.zeros([m, n]) 
   
 for i in range(1, m-1): 
     for j in range(1, n-1): 
-        temp = [img_noisy1[i-1, j-1],  
-               img_noisy1[i-1, j], 
-               img_noisy1[i-1, j + 1], 
-               img_noisy1[i, j-1], 
-               img_noisy1[i, j], 
-               img_noisy1[i, j + 1], 
-               img_noisy1[i + 1, j-1], 
-               img_noisy1[i + 1, j], 
-               img_noisy1[i + 1, j + 1]] 
+        temp = [img[i-1, j-1],  
+               img[i-1, j], 
+               img[i-1, j + 1], 
+               img[i, j-1], 
+               img[i, j], 
+               img[i, j + 1], 
+               img[i + 1, j-1], 
+               img[i + 1, j], 
+               img[i + 1, j + 1]] 
         temp = sorted(temp) 
-        img_new1[i, j]= temp[4] 
+        img_new[i, j]= temp[4] 
   
-img_new1 = img_new1.astype(np.uint8) 
-cv2.imwrite('filtroMediana.jpg', img_new1) 
+img_new = img_new.astype(np.uint8) 
+cv2.imwrite('filtroMediana.jpg', img_new) 
