a5f75d965675f1d7d2574744ae346a8b672a6e2f
tshelena <helena.tavares@sou.ucpel.edu.br>
2022-06-18 18:13:16 -0300

    adicionando normalização

---
+++ filtros.py/normalizandoImagem.py
@@ -0,0 +1,9 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("chile.jpg")
+img_new = cv2.normalize(img, None, alpha=0,beta=200, norm_type=cv2.NORM_MINMAX)
+
+img_new = img_new.astype(np.uint8) 
+cv2.imwrite('imagemNormalizada.jpg', img_new) 
+
