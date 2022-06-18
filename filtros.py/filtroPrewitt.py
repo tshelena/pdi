5bac1b96bbc0940355b733e2f0520989049527c7
tshelena <helena.tavares@sou.ucpel.edu.br>
2022-06-18 17:42:47 -0300

    -

--- filtros.py/filtroPrewitt.py
+++ filtros.py/filtroPrewitt.py
@@ -2,10 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2 
 from skimage.io import imread
-from skimage.filters import prewitt 
+from skimage.filters import prewitt
 
 img = cv2.imread("chile.jpg", cv2.IMREAD_GRAYSCALE)
 op_prewitt = prewitt(img)
 
 plt.imshow(op_prewitt, cmap=plt.cm.gray)
 plt.savefig('filtroPrewitt.jpg', format='jpg')
+
+
+
+
