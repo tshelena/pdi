56808064d12f310fa4df67af3bdc3f5698ec7f2e
tshelena <helena.tavares@sou.ucpel.edu.br>
2022-06-17 22:28:28 -0300

    adicionando filtroNegativo

--- filtroNegativo.py
+++ filtroNegativo.py
@@ -1,26 +1,19 @@
 import cv2 
 import numpy as np
 import matplotlib.pyplot as plt 
-  
-  
+   
 img = cv2.imread('chile.jpg', 1) 
 
 m, n, _ = img.shape 
- 
-img_new = np.zeros([m,n]) 
+
 for i in range(0, m - 1): 
     for j in range(0, n - 1): 
         
-        pixel = img[i, j] 
-        
-      
-        pixel[0] = 255 - pixel[0] 
-        
-        pixel[1] = 255 - pixel[1] 
-        
-        pixel[2] = 255 - pixel[2] 
-        
-        img_new[i, j] = pixel 
+        temp = img[i, j] 
+        temp[0] = 255 - temp[0] 
+        temp[1] = 255 - temp[1] 
+        temp[2] = 255 - temp[2] 
+        img[i, j] = temp 
 
-img_new = img_new.astype(np.uint8)
-cv2.imwrite('filtroNegativo.jpg', img_new)
+img = img.astype(np.uint8) 
+cv2.imwrite('filtroNegativo.jpg', img) 
