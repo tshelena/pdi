6a07df0f6718e9f06a8c7115a666352c6f4595b8
tshelena <helena.tavares@sou.ucpel.edu.br>
2022-06-19 00:59:22 -0300

    -

--- trabalho4/passaltaLaplace.py
+++ trabalho4/passaltaLaplace.py
@@ -1,8 +1,11 @@
-from PIL import Image, ImageFilter 
-  
-image = Image.open('imagem2.jpg')
-image = image.convert("L") 
-image = image.filter(ImageFilter.Kernel((3, 3), (-1, -1, -1, -1, 8, 
-                                          -1, -1, -1, -1), 1, 0)) 
-  
-image.save("passaaltaLaplace.jpg")
+import numpy as np
+import cv2
+
+img = cv2.imread('imagem2.jpg')
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+lap = cv2.Laplacian(img, cv2.CV_64F)
+
+img_new = np.vstack([lap])
+
+cv2.imwrite('passaaltaLaplace.jpg', img_new) 
+
